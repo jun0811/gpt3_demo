db42cf1a135c495a4d3a37d7cb1bb793b2ac6dff
jun0811 <nate199458@gmail.com>
2023-06-21 23:30:24 +0900

    basic component

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Article, Brand, CTA } from "./components/index";
-
-const App = () => {
-  return <div>gpt-3</div>;
-};
-
-export default App;
---
+++ src/container/whatGPT/WhatGPT.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatGPT.css";
+
+const WhatGPT = () => {
+  return <div></div>;
+};
+
+export default WhatGPT;
